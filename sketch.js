271c3faf6592d7ca5057cc5fa7ffa538004ce1cf
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2017-03-21 00:02:29 -0400

    Implement a first version of a Truchet machine

--- sketch.js
+++ sketch.js
@@ -1,7 +1,103 @@
-function setup() {
+var gridXAmount = 32 * 1;
+var gridYAmount = 18 * 1;
+var tileWidth;
+var number = 1;
+var current;
+var dark;
+var light;
+var r, g, b;
 
+function setup() {
+    createCanvas(windowWidth, windowWidth * 9 / 16);
+    background(51);
+    frameRate(10);
+    tileWidth = width / gridXAmount;
+    fill(150);
+    noStroke();
+    current = 0;
+    // noLoop();
+    dark = color(50);
+    light = color(120);
 }
 
 function draw() {
-  
+    for (var x = 0; x < width; x += tileWidth) {
+        for (var y = 0; y < height; y += tileWidth) {
+            switch (current) {
+                case 0:
+                    showA(x, y, tileWidth, light, dark);
+                    break;
+                case 1:
+                    showB(x, y, tileWidth, light, dark);
+                    break;
+                case 2:
+                    showC(x, y, tileWidth, light, dark);
+                    break;
+                case 3:
+                    showD(x, y, tileWidth, light, dark);
+                    break;
+                default:
+                    showD(x, y, tileWidth);
+            }
+            current = map(sin((x + y * gridXAmount) / number), -1, 1, 0, 4);
+            // r = map(sin((x + y * gridXAmount) / number), -1, 1, 0, 55);
+            // g = map(sin((x + y * gridXAmount) / number), -1, 1, 155, 25);
+            // b = map(cos((x + y * gridXAmount) / number), -1, 1, 150, 5);
+            // dark = color(r / 2, g / 2, b / 2);
+            // light = color(r, g, b);
+            current = floor(current);
+        }
+
+        // if (y % number == 0) {
+        //     current += 1;
+        // }
+        // if (current > 3) {
+        //     current = 0;
+        // }
+    }
+    number += 1;
+}
+
+function showA(x, y, tW, light, dark) {
+    fill(light);
+    rect(x, y, tW, tW);
+    beginShape();
+    fill(dark);
+    vertex(x, y);
+    vertex(x + tW, y + tW);
+    vertex(x, y + tW);
+    endShape();
+}
+
+function showB(x, y, tW, light, dark) {
+    fill(light);
+    rect(x, y, tW, tW);
+    beginShape();
+    fill(dark);
+    vertex(x, y);
+    vertex(x + tW, y);
+    vertex(x, y + tW);
+    endShape();
+}
+
+function showC(x, y, tW, light, dark) {
+    fill(light);
+    rect(x, y, tW, tW);
+    beginShape();
+    fill(dark);
+    vertex(x, y);
+    vertex(x + tW, y);
+    vertex(x + tW, y + tW);
+    endShape();
+}
+
+function showD(x, y, tW, light, dark) {
+    fill(light);
+    rect(x, y, tW, tW);
+    beginShape();
+    fill(dark);
+    vertex(x + tW, y);
+    vertex(x + tW, y + tW);
+    vertex(x, y + tW);
+    endShape();
 }
